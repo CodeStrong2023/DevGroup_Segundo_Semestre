9b30042186ac32ef99ccdb4d3897599d5eac3b07
Matias ikaczijk <135766814+MatiasIkaczijk@users.noreply.github.com>
2023-08-24 17:54:36 -0300

    Cargamos la clase de ayer
    Cargamos la carpeta completa con teoría y ejercicios y commiteamos en git bash también

---
+++ Clase2Ciclos/Leccion3/persona.java
@@ -0,0 +1,10 @@
+package Clase2Ciclos.Leccion3;
+
+// vamos a crear una clase(persona(valores)) pero vamos a trabajar
+// en el objeto(nombre, apellido,etc), a partir de la clase
+
+public class persona {
+    public static void main(String[] args) {
+        
+    }
+}
