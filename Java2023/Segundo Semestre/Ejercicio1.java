0d0f6c07b0eb52e25ce67df63292460a87c80a4f
Enzo Balderrama <enzoxxb@gmail.com>
2023-08-24 20:56:06 -0300

    Terminando clase 2 de java

--- Java2023/Segundo Semestre/Ejercicio1.java
+++ Java2023/Segundo Semestre/Ejercicio1.java
@@ -1,7 +1,7 @@
 // Ejercicio 1: Leer un número y mostrar su cuadrado, repetir el proceso hasta
 // que se introduzca un número negativo.
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
--- Java2023/Segundo Semestre/Ejercicio2.java
+++ Java2023/Segundo Semestre/Ejercicio2.java
@@ -3,13 +3,47 @@
  * El proceso se repetirá hasta que se ingrese el 0.
  */
 
-import java.util.Scanner;
+// import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
         float numero;
-        Scanner entrada = new Scanner(System.in);
+        // con Class Scanner
+        /* Scanner entrada = new Scanner(System.in);
         System.out.println("Digite un número: ");
         numero = Float.parseFloat(entrada.nextLine());
+        if (numero!=0){
+            do {
+                if(numero>0){
+                    System.out.println("El número ingresado es positivo.");
+                } else if(numero<0){
+                    System.out.println("El número ingresado es negativo.");
+                } else {
+                    System.out.println("Ha ingresado el 0.");
+                }
+                System.out.println("Digite otro número: ");
+                numero = Float.parseFloat(entrada.nextLine());
+            } while (numero!=0);
+        } else {
+            System.out.println("Ha ingresado el 0.");
+            
+        }*/
+        // Forma JOptionPane
+        numero = Float.parseFloat(JOptionPane.showInputDialog("Digite un número: "));
+        if (numero!=0){
+            do {
+                if(numero>0){
+                    System.out.println("El número ingresado es positivo.");
+                } else if(numero<0){
+                    System.out.println("El número ingresado es negativo.");
+                } 
+                numero = Float.parseFloat(JOptionPane.showInputDialog("Digite otro número: "));
+                } while (numero!=0);
+        }
+        if (numero==0){
+            System.out.println("Ha ingresado el 0.");
+        }
     }
 }
