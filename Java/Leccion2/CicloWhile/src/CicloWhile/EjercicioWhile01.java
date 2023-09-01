b2f62627898398ced9e4cb4b149f7a7d8c8bc3f7
gabriel.gonzalez <gabiggonzalez719@gmail.com>
2023-09-01 18:33:52 -0300

    Carpeta Java y Python

---
+++ Java/Leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -0,0 +1,33 @@
+package CicloWhile;
+
+public class EjercicioWhile01 {
+
+    public static void main(String[] args) {
+        var conteo = 0;
+        while (conteo < 3) {
+            System.out.println("conteo = " + conteo);
+            conteo++; //Vamos aumentado en uno la variable
+        }
+
+        var contador = 0;
+        do {
+            System.out.println("contador = " + contador);
+            contador++;
+        } while (contador <= 7);
+        
+        //Uso de las palabras break y continue junto a las etiquetas (labels)
+        for (var contando = 0; contando < 7; contando++) {
+            if (contando % 2 == 0) {
+                System.out.println("contando = " + contando);
+            }
+            break;
+        }
+        inicio:
+        for (var contando = 0; contando < 7; contando++) {
+            if (contando % 2 != 0) {
+                continue inicio;//Vamos a la siguiente iteracion
+            }
+            System.out.println("contando = " + contando);
+        }
+    }
+}
