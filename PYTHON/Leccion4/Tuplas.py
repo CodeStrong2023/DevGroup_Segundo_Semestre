783f4837295e68d51bb4583c6eb044e79b2db568
Santipenafiel <santipenafiel1@gmail.com>
2023-09-04 18:19:40 -0300

    agregamos ultimo ejercicio y terminamos clase 4

---
+++ PYTHON/Leccion4/Tuplas.py
@@ -0,0 +1,14 @@
+#Definimos un tupla
+cocina = ('cuchara', 'cuchillo', 'tenedor')
+print(len(cocina)) #funcion "len" sirve para saber cuantos elementos tenemos dentro de una tupla
+
+#Acceder a un elemento, para esto si usamos corchetes y no parentesis
+print(cocina[0])
+#Mostrar de manera inversa 
+print(cocina[-1])
+
+#Acceder a un rango 
+print(cocina[0:2])
+
+#ejemplo para que sea si o si una tupla necesita llevar la "," despues del primer elemento, sino es un tipo string
+verduras = ('papa') #esto no es una tupla
