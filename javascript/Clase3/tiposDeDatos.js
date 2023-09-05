98d24f942842d818e1fcfa3c7a145f748ba186d3
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-04 22:25:29 -0300

    Javascript - Clase 3: Tipos de Datos

---
+++ javascript/Clase3/tiposDeDatos.js
@@ -0,0 +1,18 @@
+//Tipos de datos en JavaScript
+/* 
+La sintaxis de comentarios es similar a la de Java
+*/
+
+var nombre = "Marcos"; //Tipo string
+console.log(nombre);
+
+var numero = 3000; //Tipo numerico
+console.log(numero);
+
+var objeto = {
+  nombre: "Marcos",
+  apellido: "Martos",
+  telefono: 218286412,
+}; //Tipo objeto
+
+console.log(objeto);
