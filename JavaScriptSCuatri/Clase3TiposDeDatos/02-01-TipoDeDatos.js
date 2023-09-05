ca3a83a4477c63eeb0a8ae60bc35b4b8215da693
Matias ikaczijk <135766814+MatiasIkaczijk@users.noreply.github.com>
2023-09-05 14:33:59 -0300

    Trabajando con JavaScript
    Tercera clase de JavaScript, trabajamos con distintos tipos de datos

---
+++ JavaScriptSCuatri/Clase3TiposDeDatos/02-01-TipoDeDatos.js
@@ -0,0 +1,24 @@
+// Tipos de datos en JavaScript
+
+/*
+La sintaxis es similar a lo que es Java
+en cuanto a comentarios
+*/
+
+// comenzamos con datos STRING
+var nombre = "Matias";
+console.log(nombre);
+
+// tipo numerico 
+var num = 3000;
+console.log(num);
+
+// tipo object
+var objeto = {
+    // dentro de la asignacion, ponemos el nombre
+    nombre : "Matias",
+    apellido : "Ikaczijk",
+    tel : "262587111"
+}
+console.log(objeto);
+
