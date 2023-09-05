78e27e7763b9df1dc67e7a36ddbe5af069bdccd8
Enzo Balderrama <enzoxxb@gmail.com>
2023-09-05 17:51:26 -0300

    Clase 2 JavaScript completa.

---
+++ JavaScript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,11 @@
+// Tipos de datos en JavaScript
+var nombre = "Enzo"; // Tipo String
+console.log(nombre);
+var numero = 3000; // Tipo Numérico
+console.log(numero);
+var objeto = {
+    nombre : "Enzo",
+    apellido : "Balderrama",
+    celular : "2625460406"
+}
+console.log(objeto);
