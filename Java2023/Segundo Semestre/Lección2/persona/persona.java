d05828e896152aeb620bfcac7d3e429934f09522
Enzo Balderrama <enzoxxb@gmail.com>
2023-09-07 13:30:21 -0300

    Terminando clase 4 Java

---
+++ Java2023/Segundo Semestre/Lección2/persona/persona.java
@@ -0,0 +1,11 @@
+package Lección2.persona;
+public class persona {
+    // Atributos de la clase.
+    String nombre;
+    String apellido;
+    // Metodos de la clase.
+    public void obtenerInformacion (){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
