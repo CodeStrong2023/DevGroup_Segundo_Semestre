8027a41a599abc73170c390fc997bda5638362af
Enzo Balderrama <enzoxxb@gmail.com>
2023-09-11 12:09:49 -0300

    Terminando clase Java

---
+++ Java2023/Segundo Semestre/Leccion4/Aritmética.java
@@ -0,0 +1,13 @@
+package Leccion4;
+
+public class Aritmética {
+    // Atributos de la clase.
+    int a;
+    int b;
+
+    // Método.
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("Resultado = " + resultado);
+    }
+}
