6cf043044be17328bde5992d429b42acbd69c9f3
Matias ikaczijk <135766814+MatiasIkaczijk@users.noreply.github.com>
2023-09-12 23:21:03 -0300

    Cargamos la clase 4 de JavaScript
    Clases completas, con commits, tags y repaso de videos

---
+++ JavaScriptSCuatri/Clase 4 Tipos de Datos Parte 2/ReutilizarVariables.js
@@ -0,0 +1,43 @@
+// Las variables son dinamicas..
+var nombre = "Matias";
+console.log(typeof nombre);
+
+nombre = 7;
+console.log(typeof nombre)
+
+// tipo numerico 
+var num = 3000;
+console.log(num);
+
+// tipo object
+var objeto = {
+    // dentro de la asignacion, ponemos el nombre
+    nombre : "Matias",
+    apellido : "Ikaczijk",
+    tel : "262587111"
+}
+console.log(objeto);
+
+// ----- 4.4 Definir un tipo clase
+
+// Tipo de dato boolean(bandera)
+var bandera = true;
+console.log(bandera);
+
+// funcion--> permiten realizar tareas, se reutilizan lineas de codigo
+function mifuntion(){}
+console.log(typeof mifuntion)
+
+// dato symbol
+var simbolo = Symbol("Mi simbolo: ")
+console.log(simbolo)
+
+// Clases: 
+class persona{
+    // nos premite crear un obj
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof persona);
