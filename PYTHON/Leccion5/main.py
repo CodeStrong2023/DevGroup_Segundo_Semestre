1ed5fa3c8f7bb26adddc3c923464af7f44260081
Santipenafiel <santipenafiel1@gmail.com>
2023-09-18 12:03:03 -0300

    Clase 4 Python

--- PYTHON/Leccion5/main.py
+++ PYTHON/Leccion5/main.py
@@ -260,7 +260,7 @@ print(diccionario2)
 seleccionArgentina ={
     10:{'Nombre':'Lionel Messi', 'Edad': 35,'Altura': 1.70, 'Precio' : '50 Millones' , 'Posicion': 'Extremo Derecho'} ,
     11:{'Nombre':'Angel Di Maria', 'Edad': 34,'Altura': 1.80, 'Precio' : '12 Millones' , 'Posicion': 'Extremo Derecho' },
-    24:{'Nombre':'Paulo Dybala', 'Edad': 28,'Altura': 1.77, 'Precio' : '35 Millones' , 'Posicion': 'Media Punta' },
+    21:{'Nombre':'Paulo Dybala', 'Edad': 28,'Altura': 1.77, 'Precio' : '35 Millones' , 'Posicion': 'Media Punta' },
     19:{'Nombre':'Nicolas Otamendi', 'Edad': 34,'Altura': 1.83, 'Precio' : '3.5 Millones' , 'Posicion': 'Defensa Central' },
     1:{'Nombre':'Franco Armani', 'Edad': 35,'Altura': 1.89, 'Precio' : '3.5 Millones' , 'Posicion': 'Portero' },
 }
@@ -316,3 +316,7 @@ print(cola)
 seRetira= cola.pop(0)
 print(f'Atendido el cliente: {seRetira}')
 print(cola)
+
+#Seguimos mostrando como recorrer un diccionario con el ciclo for
+for i in seleccionArgentina:
+     print(f'{i} -> {seleccionArgentina[i]}')
