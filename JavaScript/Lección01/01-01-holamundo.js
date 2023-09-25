43cacbce26e5d7840ad4d4b1e36d39ba8e21385a
Angelo Vellar <angelovellar24@gmail.com>
2023-09-25 18:50:16 -0300

    Commit JavaScript día 25/9/23

--- JavaScript/Lección01/01-01-holamundo.js
+++ JavaScript/Lección01/01-01-holamundo.js
@@ -1,1 +1,6 @@
-var nombre = "Angelo";
+var nombre = "Angelo ";
+var apellido = "Vellar";
+console.log(nombre+apellido);
+var nombre = "Belen";
+var apellido = "Mateo";
+console.log(nombre+apellido)
---
+++ JavaScript/Lección01/TeoriaJavaScript.js
@@ -0,0 +1,21 @@
+                
+                            //Tipos de Datos en JavaScript
+    /*
+    La sintaxis en lo que es comentarios es muy similar a la de Java
+    realmente diriamos que es identica
+    */
+
+   //Tipo Str
+   var nombre = "Ariel"; 
+   console.log(nombre);
+   //Tipo Númerico
+   var numero = 3000; 
+   console.log(numero);
+
+   var objeto = {
+        nombre : "Angelo",
+        apellido : "Vellar",
+        telefono : "262589205"
+   }
+
+   console.log(objeto);
