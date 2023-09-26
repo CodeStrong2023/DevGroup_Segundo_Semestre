48be50610e861dece21ad52869dedf577fbb05d1
Marcos Martos <marcosmartostuvi@gmail.com>
2023-09-25 22:18:22 -0300

    Javascript - Clase 6: Concatenar Cadenas

---
+++ javascript/Clase6/concatenacionDeCadenas.js
@@ -0,0 +1,15 @@
+let nombre = "Jose";
+let apellido = "Montes";
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+let nombreCompleto2 = "Marcos" + " " + "Martos";
+console.log(nombreCompleto2);
+let juntos = nombre + 219;
+console.log(juntos);
+juntos = nombre + 20 + 23;
+console.log(juntos);
+juntos = 20 + 23 + nombre;
+console.log(juntos);
+
+nombre += apellido;
+console.log(nombre);
