51c68ee48ace54641b5fca4aeee02d02fcf8e0af
Juan Pablo Quiles <juampiquiles144@gmail.com>
2023-09-26 21:45:48 -0300

    agrego archivos JS clase6

---
+++ javascript/clase6_concatCadena.js
@@ -0,0 +1,15 @@
+let nombre = "Michael";
+let apellido = "Corleone";
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+let nombreCompleto2 = "Juan" + " " + "Quiles";
+console.log(nombreCompleto2);
+let juntos = nombre + 219;
+console.log(juntos);
+juntos = nombre + 20 + 23;
+console.log(juntos);
+juntos = 20 + 23 + nombre;
+console.log(juntos);
+
+nombre += apellido;
+console.log(nombre);
