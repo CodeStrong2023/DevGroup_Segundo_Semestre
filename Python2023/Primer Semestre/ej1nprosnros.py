200f970ec61bdea05bcccb0cdcf1c7557cdbd72f
EnzoxxB <86125294+EnzoxxB@users.noreply.github.com>
2023-09-28 19:28:21 -0300

    .

--- Python2023/Primer Semestre/ej1nprosnros.py
+++
@@ -1,6 +0,0 @@
-# Calcular la suma de n numeros.
-suma = 0
-n = int(input("Digite la cantidad de números a sumarse: "))
-for i in range(1, n):
-    suma += i
-print(f"La suma de los números es: {suma}")
--- Python2023/Segundo Semestre/Ejercicio3.py
+++
@@ -1,4 +0,0 @@
-# Crear un rango de 3 a 10 pero con incremento de 2 en 2.
-numeros = range(3, 10, 2)
-for num in numeros:
-    print(num)
