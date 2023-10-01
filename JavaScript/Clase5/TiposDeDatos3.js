82910b4a0bc494686c89e42c3bfd71ddc8b3e638
GabrielGonzalez11 <135771999+GabrielGonzalez11@users.noreply.github.com>
2023-09-30 21:01:20 -0300

    Clase 5 JS: Tipos de datos parte 3

---
+++ JavaScript/Clase5/TiposDeDatos3.js
@@ -0,0 +1,16 @@
+//Tipo de dato undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+// null 
+var y = null; // null no es un tipo de dato, pero su origen es de tipo object
+console.log(typeof y);
+// Tipo de dato array y Empity string
+var autos = ["Audi", "Peugeot", "Ford", "Chevrolet"];
+console.log(autos);
+console.log(typeof autos) //Preguntamos que tipo de dato es:
+var z = '';
+console.log(z); //Esto se refiere a una cadena vacia 
+console.log(typeof z)
