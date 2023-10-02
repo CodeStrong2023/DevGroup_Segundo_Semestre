cb274c2b3642ca0eba68847f7473f4aec6c75686
Angelo Vellar <angelovellar24@gmail.com>
2023-10-02 19:08:13 -0300

    Commit Python dia 2/10/23

--- Python/Laboratorio(ll)/teoriaFunciones.py
+++ Python/Laboratorio(ll)/teoriaFunciones.py
@@ -6,3 +6,64 @@ def mi_funcion(): #Para identificar a la funcion utilizamos paréntesis
 mi_funcion() #Estamos llamando a la funcion 
 mi_funcion() # Se puede llamar a una funcion N cantidad de veces sin problemas
 #No se puede llamar a una funcion antes de definirla
+
+#Desempaquetando de listas o list Unpacking
+def show(name,lastName):
+    print(name+ " "+ lastName )
+person = ["Ariel","Betancud"]
+show(person[0], person[1]) #Pasamos uno por uno los datos de la lista a la función
+show(*person) #Esto es lo mismo que lo anterior pero le pasamos todo junto
+person1 = ("Osvaldo", "Giordanini") #Desempaquetamos a través de una tupla
+show(*person1)
+person2 = {"lastName": "Lucero", "name": "Natalia"}
+show(**person2)
+
+                            #Repaso de ciclo for
+numbers = [1,2,3,4,5,6,7,8]
+for n in numbers:
+    print(n)
+else:
+    print("Esto se terminó")
+
+                            #List comprehension, lista de compresión
+
+names = ["Paolo","Rodrigo","Lupe","Pepe"]
+alongP = [p for p in names if p[0] == "P"] #Esto regresa una nueva lista
+print(alongP)
+
+bottleC = [{"name": "Quilmes", "country": "Arg"},
+           {"name": "Corona", "country": "Mx"},
+           {"name": "Stella Artois", "country": "Belgium"},
+           ]
+Arg = [b for b in bottleC if b["country"] == "Arg"]
+print(Arg)
+print(bottleC)
+
+# Paso de Argumentos (funciones)
+def mi_funcion2(name,lastName):
+    print("Saludos a todos")
+    print(f"Nombres: {name},Apellido: {lastName}")
+mi_funcion2("Jorge", "Lucero")
+mi_funcion2("Ariel", "Betancud")
+mi_funcion2("Analia", "Pedrosa")
+
+#Palabra return en funciones
+#Creamos una función para sumar
+def sumar(a,b): 
+    return a + b
+resultado = sumar(78,22)
+#print(f"El resultado de la suma es:  {resultado}")
+print(f"El resultado de la suma es:  {sumar(55,45)}")
+
+def sumar2(a =0 , b = 0): #Le damos un valor por default
+    return a + b
+resultado = sumar2()
+print(f"resultado de la suma: {resultado}")
+print(f"resultado de la suma: {sumar2(22,66)}")
+
+#Argumentos,variables en funciones
+def listarNombres(*nombres): #Normalmente se utiliza: *args
+    for nombre in nombres: #Se va a convertir en una tupla
+        print(nombre)
+listarNombres("Lucas","Jorge","Claudia", "Rosa","Maria")
+listarNombres("Marcos","Daniel","Roxana","Romina","Carlos")
