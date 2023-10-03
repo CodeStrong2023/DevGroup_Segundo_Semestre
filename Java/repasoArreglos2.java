273e536d16e9a36a3882dfb9a86ca74b8f995c26
Angelo Vellar <angelovellar24@gmail.com>
2023-10-03 20:21:25 -0300

    Commit Java 3/10/23

--- Java/repasoArreglos2.java
+++ Java/repasoArreglos2.java
@@ -8,7 +8,7 @@ public class repasoArreglos2 {
         Scanner consola = new Scanner(System.in);
         int numeros[] = new int[10];
         int contador = 0;
-        while (contador < 10) {
+         while (contador < 10) {
             System.out.print("Digite un número: ");
             int numero = consola.nextInt();
             numeros[contador] = numero;
@@ -19,8 +19,19 @@ public class repasoArreglos2 {
               System.out.println("Indice: " +contador+ " El valor es: " + numeros[contador]);
               contador++;
         }
-      
+                    //Acá lo hacemos con el ciclo for 
+
+          /*  for( int contador = 0; contador < 10; contador++){
+                System.out.print("Digite un número: ");
+                int numero = consola.nextInt();
+                numeros[contador] = numero;
+                
         
+            }
+            */
     consola.close();
+
+            
+
     }
 }
