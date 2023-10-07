93c927d5557183a705aa91eb57deaf355fc2f921
Juan Pablo Quiles <juampiquiles144@gmail.com>
2023-10-07 00:23:21 -0300

    archivos JS clase7

---
+++ javascript/clase7_concatChain.js
@@ -0,0 +1,12 @@
+// CLASE 7
+// let y const
+let nombre2 = "Juan";
+console.log(nombre2);
+
+const apellido2 = "Quiles";
+console.log(apellido2);
+
+let x, y;
+(x = 17), (y = 21);
+let z = x + y;
+console.log(z);
---
+++ javascript/clase7_operadoresJS.js
@@ -0,0 +1,18 @@
+// Ejercicio: Encontrar números pares.
+
+let parImpar = 10;
+if (parImpar % 2 === 0) {
+  console.log("Es un numero PAR");
+} else {
+  console.log("Es un numero IMPAR");
+}
+
+// Ejercicio: Mayor o menor de edad?
+
+let edad = 21,
+  adulto = 18;
+if (edad >= adulto) {
+  console.log("Usted es una persona adulta");
+} else {
+  console.log("Usted es una persona menor de edad");
+}
