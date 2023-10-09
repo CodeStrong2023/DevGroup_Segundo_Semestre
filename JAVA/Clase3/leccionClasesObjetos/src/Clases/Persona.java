d693e3f073fbd26645c5d2d40de623e25d466214
Santipenafiel <santipenafiel1@gmail.com>
2023-10-09 10:53:00 -0300

    Clase n°4

--- JAVA/Clase3/leccionClasesObjetos/src/Clases/Persona.java
+++ JAVA/Clase3/leccionClasesObjetos/src/Clases/Persona.java
@@ -5,4 +5,11 @@ public class Persona {
     //Atributos de la clase (caracteristica)
     String nombre;
     String apellido;
+    
+    //metodos de la clase (acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre = " + nombre);
+        System.out.println("Apellido = " + apellido);
+    }
+    
 }
