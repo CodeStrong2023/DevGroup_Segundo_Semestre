73fc584b588f1792db6c5c0636a4c686b2bcb428
GabrielGonzalez11 <135771999+GabrielGonzalez11@users.noreply.github.com>
2023-10-14 15:13:31 -0300

    Clase 5 Python: Funciones y ejercicios

--- AngeloVellar/JavaScript/Lección01/01-01-holamundo.js
+++
@@ -1,6 +0,0 @@
-var nombre = "Angelo ";
-var apellido = "Vellar";
-console.log(nombre+apellido);
-var nombre = "Belen";
-var apellido = "Mateo";
-console.log(nombre+apellido)
--- AngeloVellar/JavaScript/Lección01/HolaMundo.js
+++
@@ -1,3 +0,0 @@
-var nombre = "Angelo ";
-nombre = "Hola Mundo desde JavaScript";
-console.log(nombre);
