310221d2eef25094ca0f1c8f845bdf7cd688846f
Santiago Peñafiel <santipenafiel1@gmail.com>
2023-10-16 16:08:14 -0300

    clase n°6

--- JAVA/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
+++ JAVA/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -4,8 +4,26 @@ package Operaciones;
 
 public class Aritmetica {
     //Atributos de la clase
-    int a;
+    int a;  
     int b;
+    
+    //El constructo es un metodo especial (tiene 3 objetivos: 
+        // 1-construye un objeto 
+        // 2- Reserva un espacio en memoria
+        //3- Incializa los atributos de la clase
+    public Aritmetica(){ //Constructor 1 vacio
+        System.out.println("Se esta ejecutando el constructor n°1 ");
+    }
+    
+    //estamos viendo lo que se llama sobrecarga de constructores
+    public Aritmetica(int a , int b){//constructor 2
+    this.a= a;
+    this.b= b;
+        System.out.println("Se esta ejecutando el constructor n°2");
+    }
+    
+    
+    
     
     //Metodo
     public void sumarNumeros(){
