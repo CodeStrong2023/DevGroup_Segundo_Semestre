a7a2efb8363713cab0a9c4a0ca57ab481aa93f68
EnzoxxB <86125294+EnzoxxB@users.noreply.github.com>
2023-10-17 21:17:05 -0300

    .

--- AngeloVellar/Python+/Laboratorio(ll)/ejercicio4Clase4.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 4:Sacar la raíz cuadrada de un número positivo 
-import math
-
-numero = int(input("Digite un número positivo:"))
-while numero < 0 :
-    print("Error, el número ingresado no es valido")
-    numero = int(input("Digite un número positivo:"))
-print(f"\nSu raiz cuadrada es: {math.sqrt(numero):.2f}")
--- AngeloVellar/Python+/Lección1/Clase6Ejercicio2.py
+++
@@ -1,5 +0,0 @@
-#Ejercicio 2: Determinar si es logica la operacion
-a = float(input("Escriba el valor de a:"))
-b = float(input("Escriba el valor de b:"))
-resultado = ((3 + 5 * 8 ) < 3 and ((- 6 / 3 * 4 ) + 2 < 2)) or (a > b)
-print(f"El resultado es: {resultado}")
