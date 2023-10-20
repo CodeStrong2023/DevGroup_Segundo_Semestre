c2009b9ae096eba586685d8a15419dfbbed38015
GabrielGonzalez11 <135771999+GabrielGonzalez11@users.noreply.github.com>
2023-10-20 17:53:16 -0300

    Clase 7 Python: Continuamos con similitudes entre Java y Js

---
+++ Gabriel Gonzalez/JavaScript/Clase7/SimilitudesJavaJs.js
@@ -0,0 +1,10 @@
+let x, y; //Se pueden crear varias variables dentro de una misma línea
+x = 17, y = 21; //Se puede hacer asignación de varias variables en una misma línea
+let z = x + y;
+console.log(z)
+
+let _1num = 31; //No utilizar números para inicializar el nombre de una variable
+let rompiendo = "Rompe"; //No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
