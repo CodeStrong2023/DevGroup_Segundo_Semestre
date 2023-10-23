2b2ba44413dd7c38ec77d1b74c8a3323bc1412aa
Marcos Martos <marcosmartostuvi@gmail.com>
2023-10-22 21:34:29 -0300

    Subproceso: mostrarBebidas y otros...

--- ProyectoJava/main.java
+++ ProyectoJava/main.java
@@ -57,7 +57,7 @@ public class main {
             boolean inv, int x, int total, String productos) {
     }
 
-                                    // Creamos el método opcArmar
+    // Creamos el método opcArmar
 
     public static void opcArmar(String user, String pass, String domicilio, int cel, String[] saved_user,
             String saved_pass[], String saved_dom[], int[] saved_cel, boolean inv, int x) {
@@ -83,13 +83,250 @@ public class main {
         }
 
     }
-    
+
+    // Subproceso para mostrar_carrito
+    public static void mostrar_carrito(double carro, String[] productos) {
+        String mensaje = "Los productos del carrito son:\n";
+        for (String producto : productos) {
+            mensaje += producto + "\n";
+        }
+        mensaje += "El monto del carrito es: $ " + carro;
+
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+    // Subproceso para elegir opcArmar
+        public static void opcArmar(String saved_user, String saved_pass, String saved_dom, String saved_cel, boolean inv, int x, int total, String[] productos) {
+        
+            int opcionArmar = 0;
+
+        String entrada = JOptionPane.showInputDialog("1: Mostrar las comidas disponibles\n2: Mostrar las bebidas disponibles\n3: Volver atrás");
+        opcionArmar = Integer.parseInt(entrada);
+
+        switch (opcionArmar) {
+            case 1:
+                mostrarComidas(saved_user, saved_pass, saved_dom, saved_cel, inv, x, total, productos);
+                continue;
+            case 2:
+                mostrarBebidas(saved_user, saved_pass, saved_dom, saved_cel, inv, x, total, productos);
+                continue;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Volver atrás");
+                // Agrega la lógica para volver atrás
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+        }
+
+    }
+
+    // Subproceso fin_compra
+    public static void fin_compra() {
+        JOptionPane.showMessageDialog(null, "Su pedido ha sido registrado. Recibirá el mismo en 10-20min.");
+    }
+
+    // Subproceso Carrito y productsoCarrito
+
+    /* .
+     * .
+     * .
+     */
+
+    // Subproceso 9: Mostrar comidas disponibles
     public static void mostrarComidas(String user, String pass, String domicilio, int cel, String[] saved_user,
             String saved_pass[], String saved_dom[], int[] saved_cel, boolean inv, int x) {
     }
 
+    // Subproceso 10: Mostrar bebidas disponibles (Marcos)
     public static void mostrarBebidas(String user, String pass, String domicilio, int cel, String[] saved_user,
             String saved_pass[], String saved_dom[], int[] saved_cel, boolean inv, int x) {
+
+        // Declarar variables
+
+        int i, j, opcion, carro, opcEnvio;
+        String bebidaElegida = "";
+        double precio = 0;
+        String[][] bebidas = new String[4][4];
+        double[][] precioBebidas = new double[4][4];
+        String dom_inv = "";
+
+        // Llenamos la matriz bebidas
+        bebidas[0][0] = "1) Cerveza: Quilmes";
+        bebidas[0][1] = "2) Cerveza: Brahmita";
+        bebidas[0][2] = "3) Cerveza: Andes origen negra";
+        bebidas[0][3] = "4) Cerveza: Andes origen rubia";
+        bebidas[1][0] = "5) Cerveza: Andes origen roja";
+        bebidas[1][1] = "6) Cerveza: Andes origen ipa";
+        bebidas[1][2] = "7) Cerveza: Coronita";
+        bebidas[1][3] = "8) Cerveza: Stella";
+        bebidas[2][0] = "9) Cerveza: Salta cautiva";
+        bebidas[2][1] = "10) Vino: Chardonnay";
+        bebidas[2][2] = "11) Vino: Blanco dulce";
+        bebidas[2][3] = "12) Vino: Rosé";
+        bebidas[3][0] = "13) Vino: Malbec Roble";
+        bebidas[3][1] = "14) Vino: Cabernet sauvignon";
+        bebidas[3][2] = "15) Vino: Espumoso";
+        bebidas[3][3] = "16) Vino: Malbec";
+
+        // Llenamos la matriz precio de bebidas
+        precioBebidas[0][0] = 300;
+        precioBebidas[0][1] = 250;
+        precioBebidas[0][2] = 400;
+        precioBebidas[0][3] = 400;
+        precioBebidas[1][0] = 400;
+        precioBebidas[1][1] = 400;
+        precioBebidas[1][2] = 500;
+        precioBebidas[1][3] = 350;
+        precioBebidas[2][0] = 300;
+        precioBebidas[2][1] = 1000;
+        precioBebidas[2][2] = 800;
+        precioBebidas[2][3] = 750;
+        precioBebidas[3][0] = 900;
+        precioBebidas[3][1] = 1200;
+        precioBebidas[3][2] = 1300;
+        precioBebidas[3][3] = 850;
+
+          // Mostrar lista de bebidas y precios
+        StringBuilder bebidasTexto = new StringBuilder();
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                bebidasTexto.append(bebidas[i][j]).append(" $ ").append(precioBebidas[i][j]).append("\n");
+            }
+        }
+        JOptionPane.showMessageDialog(null, bebidasTexto.toString(), "Bebidas Disponibles", JOptionPane.INFORMATION_MESSAGE);
+
+        // Leer la opción
+        opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la opción que desee:"));
+
+           switch (opcion) {
+            case 1:
+                bebidaElegida = "Cerveza: Quilmes";
+                precio += precioBebidas[0][0];
+                break;
+            case 2:
+                bebidaElegida = "Cerveza: Brahmita";
+                precio += precioBebidas[0][1];
+                break;
+            case 3:
+                bebidaElegida = "Cerveza: Andes origen negra";
+                precio += precioBebidas[0][2];
+                break;
+            case 4:
+                bebidaElegida = "Cerveza: Andes origen rubia";
+                precio += precioBebidas[0][3];
+                break;
+            case 5:
+                bebidaElegida = "Cerveza: Andes origen roja";
+                precio += precioBebidas[1][0];
+                break;
+            case 6:
+                bebidaElegida = "Cerveza: Andes origen ipa";
+                precio += precioBebidas[1][1];
+                break;
+            case 7:
+                bebidaElegida = "Cerveza: Coronita";
+                precio += precioBebidas[1][2];
+                break;
+            case 8:
+                bebidaElegida = "Cerveza: Stella";
+                precio += precioBebidas[1][3];
+                break;
+            case 9:
+                bebidaElegida = "Cerveza: Salta cautiva";
+                precio += precioBebidas[2][0];
+                break;
+            case 10:
+                bebidaElegida = "Vino: Chardonnay";
+                precio += precioBebidas[2][1];
+                break;
+            case 11:
+                bebidaElegida = "Vino: Blanco dulce";
+                precio += precioBebidas[2][2];
+                break;
+            case 12:
+                bebidaElegida = "Vino: Rosé";
+                precio += precioBebidas[2][3];
+                break;
+            case 13:
+                bebidaElegida = "Vino: Malbec Roble";
+                precio += precioBebidas[3][0];
+                break;
+            case 14:
+                bebidaElegida = "Vino: Cabernet sauvignon";
+                precio += precioBebidas[3][1];
+                break;
+            case 15:
+                bebidaElegida = "Vino: Espumoso";
+                precio += precioBebidas[3][2];
+                break;
+            case 16:
+                bebidaElegida = "Vino: Malbec";
+                precio += precioBebidas[3][3];
+                break;
+        }
+      
+         // Muestra el resultado en una ventana emergente
+        JOptionPane.showMessageDialog(null, "Se ha añadido al carrito: " + bebidaElegida, "Bebida Agregada", JOptionPane.INFORMATION_MESSAGE);
+
+        // Definimos opcCarrito como string vacío
+        String opcCarrito = "";
+
+        
+        do {
+            opcCarrito = JOptionPane.showInputDialog("¿Desea agregar algo más al carrito? (S/N)").toUpperCase();
+
+            if (opcCarrito.equals("S")) {
+                carro = Carrito(total, precio);
+                productos = ProductosCarrito(productos, bebidaElegida);
+                mostrar_carrito(carro, productos);
+                opcArmar(saved_user, saved_pass, saved_dom, saved_cel, inv, x, total, productos);
+            } else {
+                if (opcCarrito.equals("N")) {
+                    carro = Carrito(total, precio);
+                    productos = ProductosCarrito(productos, bebidaElegida);
+                    mostrar_carrito(carro, productos);
+
+                    String[] opciones = {"Retirar en el local", "Envío a domicilio"};
+                    opcEnvio = JOptionPane.showOptionDialog(
+                            null,
+                            "Desea...",
+                            "Opciones de Envío",
+                            JOptionPane.DEFAULT_OPTION,
+                            JOptionPane.QUESTION_MESSAGE,
+                            null,
+                            opciones,
+                            opciones[0]
+                    );
+
+                    if (opcEnvio == 1) {
+                        JOptionPane.showMessageDialog(null, "Ha seleccionado -Envío a Domicilio-\nEl costo de envío es de $200.\nEl monto total es de: $" + (carro + 200), "Envío a Domicilio", JOptionPane.INFORMATION_MESSAGE);
+
+                        if (inv) {
+                            dom_inv = JOptionPane.showInputDialog("Por Favor Ingrese su domicilio:");
+                            fin_compra();
+                        } else {
+                            if (saved_dom[x].isEmpty()) {
+                                saved_dom[x] = JOptionPane.showInputDialog("Domicilio No cargado. Por Favor Ingrese su domicilio:");
+                                fin_compra();
+                            } else {
+                                fin_compra();
+                            }
+                        }
+                    } else {
+                        if (opcEnvio == 0) {
+                            JOptionPane.showMessageDialog(null, "Ha seleccionado -Retirar en el local-\nEl monto total es de: $" + carro, "Retirar en el Local", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Error. Opción no válida. Ingrese nuevamente.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        } while (opcCarrito.equals("S") || opcCarrito.equals("N"));
+
     }
 
-}
+     
+
+
+
+    }
