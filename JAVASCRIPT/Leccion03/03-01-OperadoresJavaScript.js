4e66758fd9ec4afaf63c4678d31c2d701f76154e
Santiago Peñafiel <santipenafiel1@gmail.com>
2023-10-23 03:00:31 -0300

    clase 6 y 7 JavaScript

---
+++ JAVASCRIPT/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar numeros pares e impares
+let parImpar = 1;
+if(parImpar %2 == 0){
+    console.log("Es un numero Par")
+    }
+    else { 
+        console.log("Es un numero Impar")
+    }
+
+    //Ejercicio: Es mayor de edad
+    let edad = 18, adulto = 18;
+    if(edad >= adulto) { 
+        console.log ("Usted es una persona adulta")
+    }
+    else {
+        console.log("Usted es una persona menor de edad")
+    }
