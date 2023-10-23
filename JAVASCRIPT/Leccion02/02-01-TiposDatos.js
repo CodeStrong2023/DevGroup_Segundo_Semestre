11f960b6706aaf34d4b70e1b9050319956b4ddd4
Santiago Peñafiel <santipenafiel1@gmail.com>
2023-10-23 02:10:19 -0300

    Clase 5 JavaScript Similitudes entre Java y JavaScript

--- JAVASCRIPT/Leccion02/02-01-TiposDatos.js
+++ JAVASCRIPT/Leccion02/02-01-TiposDatos.js
@@ -41,3 +41,22 @@ class Persona{
     }
 }
 console.log(typeof Persona);
+
+//Tipo de dato undefined 
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(typeof x);
+// null: significa ausencia de valor
+var y = null; //null no es un tipo de dato, pero su origen es de tipo object
+console.log(typeof y);
+
+//Tipo de dato array y Empty String
+var autos = ['Citroen','Audi','Bmw','Ford'];
+console.log(autos);
+console.log(typeof autos); //preguntamos que tipo de datos es
+
+var z = '';
+console.log(z); //Esto se refiere a que es una cadena vacia:
+console.log(typeof z);
