26e64f132869784a544803d9ddeaf2f1edf6a5b0
Enzo Balderrama <enzoxxb@gmail.com>
2023-10-25 21:02:41 -0300

    Clase 7 JS

--- JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -12,3 +12,15 @@ juntos = 78 + 17 + nombre;
 console.log(juntos);
 nombre += apellido;
 console.log(nombre);
+
+// Ya no usamos var, usamos let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lopez";
+console.log(apellido2);
+
+let x, y; // Se pueden crear varias variables dentro de una misma linea
+(x = 17), (y = 21); // Se puede asignar las variables dentro de la misma linea
+let z = x + y; // Se asigna el valor de la operación.
+console.log(z);
