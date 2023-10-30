226ee4bf79c120e5a3f9f1a2359e15b9ee87133c
Santiago Peñafiel <santipenafiel1@gmail.com>
2023-10-30 20:03:08 -0300

    Agrego el MENU

--- ProyectoJava/main.java
+++ ProyectoJava/main.java
@@ -93,6 +93,33 @@ public class main {
 
     private static void menu(String[] saved_user, String[] saved_pass, String[] saved_dom, int[] saved_cel,
             boolean inv, int x, int total, String productos) {
+        
+        if (inv = false) {
+         JOptionPane.showMessageDialog(null, "Bienvenido: " + saved_user[x]);
+         }
+        else {
+            JOptionPane.showMessageDialog(null, "Bienvenido ");
+            JOptionPane.showMessageDialog(null, "\n Ha ingresado como invitado ");          
+        }
+        JOptionPane.showMessageDialog(null, "\n       MENU         ");
+        
+        //creamos las variables y se inicializan en 0 por defecto
+        int opcion, opcionComprar;
+        JOptionPane.showMessageDialog(null, " ");
+        //mostrar menu
+        JOptionPane.showMessageDialog(null, "1: Comprar un combo");
+        JOptionPane.showMessageDialog(null, "2: Armar Pedido");
+        opcion = Integer.parseInt(JOptionPane.showInputDialog("\nIngrese una opcion"));
+        //tras elegir una opcion
+               
+        switch(opcion) {
+            case 1:
+                opcComprar(saved_user,saved_pass,saved_dom,saved_cel,inv,x,total,productos);
+            break;
+            case 2:
+                opcArmar(saved_user,saved_pass,saved_dom,saved_cel,inv,x,total,productos);          
+            break;
+            }    
     }
 
     // Creamos el método opcArmar
