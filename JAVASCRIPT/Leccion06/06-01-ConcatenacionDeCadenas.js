f42fe698ea44a798c3cc100a2dedc50c5d07f922
JEsposito95 <joaquiesposito@gmail.com>
2023-10-31 19:13:12 -0300

    leccion 7 de js

---
+++ JAVASCRIPT/Leccion06/06-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,20 @@
+var nombre= 'Jose';
+var apellido = 'Monte';
+var nombreCompleto= nombre+' '+apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2= 'Joaquin'+ ' '+ 'Esposito';
+console.log(nombreCompleto2);
+
+var juntos= nombre + 219;
+console.log(juntos);
+
+juntos= nombre + 78 + 17;
+console.log(jnutos);
+
+juntos= 78+17+ nombre;
+console.log(juntos);
+//
+
+nombre += apellido;
+console.log(nombre);
