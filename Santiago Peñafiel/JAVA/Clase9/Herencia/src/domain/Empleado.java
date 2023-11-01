7fba3d1ecb812df74dcbed817c395bcbd52a2386
Santiago Peñafiel <santipenafiel1@gmail.com>
2023-11-01 17:45:56 -0300

    Clase 10 POO sobrecarga de constructores y metodos en JAVA

--- Santiago Peñafiel/JAVA/Clase9/Herencia/src/domain/Empleado.java
+++ Santiago Peñafiel/JAVA/Clase9/Herencia/src/domain/Empleado.java
@@ -7,10 +7,15 @@ public class Empleado extends Persona{ //con extends, decimos de donde va a here
     private double sueldo;
     private static int contadorEmpleados;//es para incrementar
 
-    //Constructor
-    public Empleado(String nombre, double sueldo) {
-        super(nombre);
-        this.idEmpleado = ++Empleado.contadorEmpleados;
+    //Constructores
+    public Empleado(){ //Constructor 1
+         this.idEmpleado = ++Empleado.contadorEmpleados;
+    }
+    
+    public Empleado(String nombre, double sueldo) { //Constructor 2
+        //super(nombre); //si usamos super, no podemos usar this para llamar a un constructor interno
+        this(); //EStamos llamando desde aqui al constructor vacio (llamar a un constructor interno)
+        this.nombre = nombre;
         this.sueldo = sueldo;
     }
 
