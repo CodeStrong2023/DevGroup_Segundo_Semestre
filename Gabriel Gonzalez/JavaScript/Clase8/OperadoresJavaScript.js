e3976cc5711b2442c0adf1f6eac84e77c3667b83
GabrielGonzalez11 <135771999+GabrielGonzalez11@users.noreply.github.com>
2023-10-31 22:34:09 -0300

    Clase 8 JS: var, let y const

---
+++ Gabriel Gonzalez/JavaScript/Clase8/OperadoresJavaScript.js
@@ -0,0 +1,27 @@
+//Ejercicio para encontrar números pares
+let parImpar = 11;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR")
+}
+else{
+    console.log("Es un número IMPAR")
+}
+
+//Ejercicio es mayor de edad
+let Edad = 19, mayorDeEdad = 18;
+if(Edad >= mayorDeEdad){
+    console.log("Es mayor de edad")
+}
+else{
+    console.log("Es menor de edad")
+}
+
+//Ejercicio dentro de un rango
+let dentroRango = 10;
+let valMinima = 0, valMaxima = 10;
+if(dentroRango >= valMinima && dentroRango <= valMaxima){
+    console.log("Esta dentro del rango establecido");
+}
+else{
+    console.log("Esta fuera de rango del rango establecido");
+}
