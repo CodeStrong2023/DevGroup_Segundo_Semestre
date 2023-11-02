5516c43b9ad1b6bf228214a98061f641fb3e1aff
Angelo Vellar <angelovellar24@gmail.com>
2023-11-02 20:22:52 -0300

    Agregando cambios en Java

---
+++ Angelo Vellar/Java/Programación(ll)/Proyectos(POO)/Clase 12/Matrices/src/test/TestMatrices.java
@@ -0,0 +1,29 @@
+
+package test;
+
+public class TestMatrices {
+    public static void main(String[] args) {
+        int edades[][] = new int[3][3];
+        System.out.println("edades = " + edades);
+        edades [0][0]  =  5; //Llenado manual
+        edades [0][1] = 7; //Es una diferente columna
+        edades [1][0] = 8;
+        edades [1][1] = 4;
+        edades [2][0] = 9;
+        edades [2][1] = 2;
+        edades [2][2] = 6;
+        System.out.println("edades 0-0 = " + edades[0][0]);
+        System.out.println("edades 0-1 = " + edades[0][1]);
+        System.out.println("edades 1-0 = " + edades[1][0]);
+        System.out.println("edades 1-1 = " + edades[1][1]);
+        System.out.println("edades 2-0 = " + edades[2][0]);
+        System.out.println("edades 2-1 = " + edades[2][1]);
+        System.out.println("edades 2-2 = " + edades[2][2]);
+        System.out.println("Recorremos la matriz através del ciclo for");
+        for (int fila = 0; fila < edades.length; fila++) {
+            for (int col = 0; col < edades[fila].length; col++) {
+                System.out.println("edades = " + fila+"-"+col+": "+edades[fila][col]);
+            }
+        }
+    }
+}
