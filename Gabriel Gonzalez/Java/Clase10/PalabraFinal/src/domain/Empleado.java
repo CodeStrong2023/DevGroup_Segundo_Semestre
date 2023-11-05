d5415837eb48c9b94f17ee1d925fd3ed7585e878
GabrielGonzalez11 <135771999+GabrielGonzalez11@users.noreply.github.com>
2023-11-04 22:04:21 -0300

    Clase 10 Java: Sobrecarga de constructores y métodos + palabra final

---
+++ Gabriel Gonzalez/Java/Clase10/PalabraFinal/src/domain/Empleado.java
@@ -0,0 +1,8 @@
+
+package domain;
+
+public class Empleado extends Persona {
+    public void imprimir(){
+        System.out.println("Método de imprimir desde la clase hija");
+    }
+}
---
+++ Gabriel Gonzalez/Java/Clase10/Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,12 @@
+
+package test;
+
+//import ar.com.codesystem.Utileria.*;
+import ar.com.codesystem.Utileria;
+//import static ar.com.codesystem.Utileria.imprimir; //Solo aplica para métodos staticos
+public class TestUtileria {
+    public static void main(String[] args) {
+        Utileria.imprimir("Saludo al profesor y tutores de la tecnicatura");
+        //ar.com.codesystem.Utileria.imprimir("Ahora si estamos terminando");
+    }
+}
