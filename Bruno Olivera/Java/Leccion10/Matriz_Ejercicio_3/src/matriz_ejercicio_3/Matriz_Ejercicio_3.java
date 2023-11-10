3928f54154d989fd6540dfc4afed60b3b23f0e55
BrunoOlivera8 <brunoolivera99.bo@gmail.com>
2023-11-09 22:14:49 -0300

    commit
    commits varios

--- Bruno Olivera/Java/Leccion10/Matriz_Ejercicio_3/src/matriz_ejercicio_3/Matriz_Ejercicio_3.java
+++ Bruno Olivera/Java/Leccion10/Matriz_Ejercicio_3/src/matriz_ejercicio_3/Matriz_Ejercicio_3.java
@@ -3,10 +3,39 @@
  */
 package matriz_ejercicio_3;
 
+import java.util.Scanner;
+
 public class Matriz_Ejercicio_3 {
 
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+        int matriz[][] = new int[3][3];
+        System.out.println("Rellenar la matriz: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Matriz [" + i + "] [" + j + "]");
+                matriz[i][j] = entrada.nextInt();
+            }
+            System.out.println("");
+        }
+        //matriz original
+        System.out.println("Matriz original: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        //matriz transpuesta
+        System.out.println("Matriz transpuesta: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matriz[j][i] + " ");
+            }
+            System.out.println("");
+        }
     }
-    
+
 }
